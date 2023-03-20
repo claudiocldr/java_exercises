af0277ebd0d56998093207e6e92cd0f7cbff37c5
claudiocldr <claudiocaldarella13@gmail.com>
2023-03-20 17:33:58 +0100

    added exercise oop 2

---
+++ javaoop/exercises/_2/Student.java
@@ -0,0 +1,25 @@
+package javabasics.javaoop.exercises._2;
+
+public class Student {String name;
+    int age;
+
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student(int age) {
+        this.age = age;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student () {
+
+    }
+
+
+}
